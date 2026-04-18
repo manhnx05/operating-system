15a98331b65f62c8c0da5e45e066896d2c9bb643
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-04-18 23:25:31 +0700

    feat: Implement string utility functions (strlen, strcpy, strcmp)

---
+++ kernel/string.c
@@ -0,0 +1,29 @@
+#include "string.h"
+
+int strlen(const char* str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+void strcpy(char* dest, const char* src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+int strcmp(const char* s1, const char* s2) {
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
+        i++;
+    }
+    return s1[i] - s2[i];
+}
